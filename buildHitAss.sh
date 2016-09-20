8f70d4dd6f7c39e527c4a72b9d36e3ab30f91115
Georg Federmann <georg.federmann@gmail.com>
2016-09-20 13:44:57 +0200

    Enhance build script

--- buildHitAss.sh
+++ buildHitAss.sh
@@ -13,6 +13,7 @@ print_help() {
     echo "Default action is to execute maven builds and copy the artefacts to specified locations on the file system."
     echo
     echo "t  build grammar tools package and deploy it to $TOOLS_TARGET_PATH"
+    echo "d  build grammar tools package and deploy it to NEXUS snapshot repository"
     echo "h  print this help"
 }
 
@@ -23,6 +24,13 @@ case $1 in
     ./createHitAssAstParser.sh
     popd
     mvn -Pprod -DargLine="-Dhit2ass.clou.path=$HIT_CLOU_LIBRARY_PATH" clean package assembly:single && cp ${TARGET_FOLDER}${TOOLS_JAR_NAME} ${TOOLS_TARGET_PATH}
+    ;;
+  d)
+    echo "Building hit/ass tools package and deploying to NEXUS snapshot repository"
+    pushd $JAVACC_PATH
+    ./createHitAssAstParser.sh
+    popd
+    mvn -Pprod -DargLine="-Dhit2ass.clou.path>=$HIT_CLOU_LIBRARY_PATH" clean package assembly:single deploy && cp ${TARGET_FOLDER}${TOOLS_JAR_NAME} ${TOOLS_TARGET_PATH}
     ;;
   h)
     print_help
